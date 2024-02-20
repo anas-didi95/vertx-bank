463f6244c49f324706d9829194f1ce442d3720e5
Anas Juwaidi <anas.didi95@gmail.com>
2024-02-20 10:19:07 +0800

    REF: Refactor code

--- ms-bank-svc/src/main/java/com/anasdidi/msbanksvc/common/BaseRoute.java
+++ ms-bank-svc/src/main/java/com/anasdidi/msbanksvc/common/BaseRoute.java
@@ -1,16 +1,22 @@
 package com.anasdidi.msbanksvc.common;
 
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
 public abstract class BaseRoute {
 
+  @SuppressWarnings("unused")
+  private EventBus eventBus;
+
   protected abstract JsonObject process(RoutingContext ctx);
 
   protected abstract HttpMethod getHttpMethod();
 
   protected abstract String getPath();
+
+  protected abstract boolean hasEventBus();
 
   protected abstract BaseDTO validate(RoutingContext ctx);
 
@@ -25,5 +31,9 @@ public abstract class BaseRoute {
   protected final void handle(RoutingContext ctx) {
     JsonObject responseBody = process(ctx);
     ctx.response().end(responseBody.encode());
+  }
+
+  protected final void setEventBus(EventBus eventBus) {
+    this.eventBus = eventBus;
   }
 }
