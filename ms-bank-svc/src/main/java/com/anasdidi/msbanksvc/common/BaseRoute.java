4ce57756579c82c4a00685fa9cb9b62d068ae390
Anas Juwaidi <anas.didi95@gmail.com>
2024-03-17 12:34:01 +0800

    ADD: Add test

--- ms-bank-svc/src/main/java/com/anasdidi/msbanksvc/common/BaseRoute.java
+++ ms-bank-svc/src/main/java/com/anasdidi/msbanksvc/common/BaseRoute.java
@@ -2,6 +2,7 @@ package com.anasdidi.msbanksvc.common;
 
 import com.anasdidi.msbanksvc.exception.ValidationException;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Future;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpMethod;
@@ -20,6 +21,8 @@ public abstract class BaseRoute {
 
   protected abstract String getPath();
 
+  protected abstract HttpResponseStatus getHttpResponseStatus();
+
   protected abstract BaseDTO getRequestVariable(RoutingContext ctx) throws ValidationException;
 
   protected final BaseDTO getDTO(RoutingContext ctx) {
@@ -28,7 +31,7 @@ public abstract class BaseRoute {
 
   protected final void handle(RoutingContext ctx, Pool pool) {
     pool.withTransaction(conn -> process(ctx, conn))
-        .onSuccess(body -> ctx.response().end(body.encode()))
+        .onSuccess(body -> ctx.response().setStatusCode(getHttpResponseStatus().code()).end(body.encode()))
         .onFailure(ctx::fail);
   }
 
